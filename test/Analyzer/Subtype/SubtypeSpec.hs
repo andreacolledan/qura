4db415f8cd4a4bfdcc0325ab20d370a59b94f90a
Andrea Colledan <andreacolledan@fastmail.com>
2025-08-03 19:35:35 +0200

    Tests now use system-agnostic paths

--- test/Analyzer/Subtype/SubtypeSpec.hs
+++ test/Analyzer/Subtype/SubtypeSpec.hs
@@ -3,18 +3,28 @@ module Analyzer.Subtype.SubtypeSpec (spec) where
 import Test.Hspec
 import TestUtil
 import Control.Monad
+import System.FilePath
+
+programDir :: FilePath
+programDir = "test" </> "Analyzer" </> "Subtype"
+
+positiveDir :: FilePath
+positiveDir = programDir </> "pos"
+
+negativeDir :: FilePath
+negativeDir = programDir </> "neg"
 
 spec :: Spec
 spec = do
   around (withSolver Nothing) $ do
     describe "subtype checker" $ do
       it "accepts correct programs" $ \qfh -> do
-        withTests "test/Analyzer/Subtype/pos/" $ \tests -> do
+        withTests positiveDir $ \tests -> do
           when (length tests == 0) pending
           forM_ tests $ \test -> do
             widthCheckingTest [] qfh `shouldAccept` test
       it "rejects incorrect programs" $ \qfh -> do
-        withTests "test/Analyzer/Subtype/neg/" $ \tests -> do
+        withTests negativeDir $ \tests -> do
           when (length tests == 0) pending
           forM_ tests $ \test -> do
             widthCheckingTest [] qfh `shouldReject` test
