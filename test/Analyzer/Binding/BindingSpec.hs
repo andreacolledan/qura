45ed3d1c16c0823116d587af82a3e580f89ef1ef
Andrea Colledan <andreacolledan@fastmail.com>
2025-08-03 19:35:35 +0200

    Tests now use system-agnostic paths

--- test/Analyzer/Binding/BindingSpec.hs
+++ test/Analyzer/Binding/BindingSpec.hs
@@ -3,18 +3,28 @@ module Analyzer.Binding.BindingSpec (spec) where
 import Test.Hspec
 import TestUtil
 import Control.Monad
+import System.FilePath
+
+programDir :: FilePath
+programDir = "test" </> "Analyzer" </> "Binding"
+
+positiveDir :: FilePath
+positiveDir = programDir </> "pos"
+
+negativeDir :: FilePath
+negativeDir = programDir </> "neg"
 
 spec :: Spec
 spec = do
   around (withSolver Nothing) $ do
     describe "binding checker" $ do
       it "accepts correct programs" $ \qfh -> do
-        withTests "test/Analyzer/Binding/pos/" $ \tests -> do
+        withTests positiveDir $ \tests -> do
           when (length tests == 0) pending
           forM_ tests $ \test -> do
             typeCheckingTest [] qfh `shouldAccept` test
       it "rejects incorrect programs" $ \qfh -> do
-        withTests "test/Analyzer/Binding/neg/" $ \tests -> do
+        withTests negativeDir $ \tests -> do
           when (length tests == 0) pending
           forM_ tests $ \test -> do
             typeCheckingTest [] qfh `shouldReject` test
--- test/Analyzer/Linearity/LinearitySpec.hs
+++ test/Analyzer/Linearity/LinearitySpec.hs
@@ -3,18 +3,28 @@ module Analyzer.Linearity.LinearitySpec (spec) where
 import Test.Hspec
 import TestUtil
 import Control.Monad
+import System.FilePath
+
+programDir :: FilePath
+programDir = "test" </> "Analyzer" </> "Linearity"
+
+positiveDir :: FilePath
+positiveDir = programDir </> "pos"
+
+negativeDir :: FilePath
+negativeDir = programDir </> "neg"
 
 spec :: Spec
 spec = do
   around (withSolver Nothing) $ do
     describe "linearity checker" $ do
       it "accepts correct programs" $ \qfh -> do
-        withTests "test/Analyzer/Linearity/pos/" $ \tests -> do
+        withTests positiveDir $ \tests -> do
           when (length tests == 0) pending
           forM_ tests $ \test -> do
             typeCheckingTest [] qfh `shouldAccept` test
       it "rejects incorrect programs" $ \qfh -> do
-        withTests "test/Analyzer/Linearity/neg/" $ \tests -> do
+        withTests negativeDir $ \tests -> do
           when (length tests == 0) pending
           forM_ tests $ \test -> do
             typeCheckingTest [] qfh `shouldReject` test
