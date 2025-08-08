e8cf0446d80a3792ac532c5bc25fca9867244ebd
Davide Sonno <90336130+davidesonno@users.noreply.github.com>
2025-08-08 11:24:04 +0200

    Update Interpreter.hs
    - fixed definition search

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -61,7 +61,7 @@ splitTLDEFs [x] = ([], Just x)
 splitTLDEFs xs = (init xs, Just (last xs))
 
 mergeModLibs :: Module -> [Module] -> Either RuntimeError Expr
-mergeModLibs (Module n e i defs) libs = do
+mergeModLibs (Module programName e i defs) libs = do
   -- for now I assume no dependencies inside the libraries,
   -- and of course no cross dependencies between the libraries.
 
@@ -70,126 +70,162 @@ mergeModLibs (Module n e i defs) libs = do
   -- From this 'start', substitute with the maps, being careful with the
   -- var names
   let (remaining, start) = splitTLDEFs defs
-  let definitionsMap = createMapFromModules ((Module n e i remaining) : libs)
+  let definitionsMap = createMapFromModules ((Module programName e i remaining) : libs)
 
   case start of
     Just (TopLevelDefinition startId startArgs startSign sartDef) -> 
-      -- let s="dumbNot" in
       trace ( ""
         ++"---- start:\n"++(show (TopLevelDefinition startId startArgs startSign sartDef))
         ++"\n---- def map:\n"++(show definitionsMap)
       --   -- ++"-- start:\n"++(prettyTopLevelDefinition (TopLevelDefinition startId startArgs signature sartDef))
       --   -- ++"\n"++(pretty definitionsMap)
-      --   -- ++"\n-- example "++s++" search:\n"++show (searchDefinition definitionsMap s)
         ) $ 
     -- substitute in the starting tldef using the maps
-      case applyModulesMap definitionsMap sartDef of
+      case applyModulesMap definitionsMap (programName, startId, sartDef) of
         Right expr -> Right $ wrapExpr expr startArgs startSign
         Left err -> Left err
 
     Nothing -> Left (RuntimeError "No definitions in the input module")
   
-applyModulesMap :: ModulesMap -> Expr -> Either RuntimeError Expr
-applyModulesMap maps t 
-  | M.null maps = Right t -- not really needed but would save some time
-  | otherwise = case t of
+applyModulesMap :: ModulesMap -> (String, VariableId, Expr) -> Either RuntimeError Expr
+applyModulesMap maps (progName, startId, startDef) 
+  | M.null maps = Right startDef -- not really needed but would save some time
+  | otherwise = case startDef of
     EUnit -> Right EUnit
 
-    EVar x -> case searchDefinition maps x of
+    EVar x -> case searchDefinition maps progName startId x of
       Left err -> Left err
 
-      Right (Just (TopLevelDefinition tldefId tldefArgs tldefSign tldefExpr)) -> -- definition found
+      Right (Just (sourceModule, (TopLevelDefinition tldefId tldefArgs tldefSign tldefExpr))) -> -- definition found
         do 
-          -- also apply the modules to the found def
-          tldefExpr' <- applyModulesMap maps tldefExpr
-          -- wrap the def with abstractions for its vars
+          -- Since that we are assuming no dependencies in the lib, for now we only recurse
+          -- if the definition was found in the user program (so the module progName)
+          tldefExpr' <- if sourceModule == progName
+            then applyModulesMap maps (progName, tldefId, tldefExpr)
+            else pure tldefExpr
+
+          -- wrap the definition with abstractions for its vars
           let tldefExpr'' = wrapExpr tldefExpr' tldefArgs tldefSign
+
           -- finally, return the lifted function
           Right (ELift tldefExpr'')
 
-      Right Nothing -> Right t -- No definition found, return the term itself
+      Right Nothing -> Right startDef -- No definition found, return the term itself
 
     ETuple exprs -> do
-      exprs' <- mapM (\e -> applyModulesMap maps e) exprs
+      exprs' <- mapM (\e -> applyModulesMap maps (progName, startId, e)) exprs
       Right (ETuple exprs')
 
     EAbs ptrn typ expr -> do
-      expr' <- applyModulesMap maps expr
+      expr' <- applyModulesMap maps (progName, startId, expr)
       Right (EAbs ptrn typ expr')
 
     ELift expr -> do
-      expr' <- applyModulesMap maps expr
+      expr' <- applyModulesMap maps (progName, startId, expr)
       Right (ELift expr')
 
     ENil typ -> Right (ENil typ)
 
     ECons expr1 expr2 -> do
-      expr1' <- applyModulesMap maps expr1
-      expr2' <- applyModulesMap maps expr2
+      expr1' <- applyModulesMap maps (progName, startId, expr1)
+      expr2' <- applyModulesMap maps (progName, startId, expr2)
       Right (ECons expr1' expr2')
 
     EFold _ _ _ -> Left (RuntimeError "EFold not supported yet")
-    -- EFold expr1 expr2 t3 -> EFold (applyModulesMap maps expr1) (applyModulesMap maps expr2) (applyModulesMap maps t3)
 
     EApp expr1 expr2 -> do
-      expr1' <- applyModulesMap maps expr1
-      expr2' <- applyModulesMap maps expr2
+      expr1' <- applyModulesMap maps (progName, startId, expr1)
+      expr2' <- applyModulesMap maps (progName, startId, expr2)
       Right (EApp expr1' expr2')
 
     EApply expr1 expr2 -> do
-      expr1' <- applyModulesMap maps expr1
-      expr2' <- applyModulesMap maps expr2
+      expr1' <- applyModulesMap maps (progName, startId, expr1)
+      expr2' <- applyModulesMap maps (progName, startId, expr2)
       Right (EApply expr1' expr2')
 
     EBox _ _ -> Left (RuntimeError "EBox not supported yet")
 
     EForce expr -> do
-      expr' <- applyModulesMap maps expr
+      expr' <- applyModulesMap maps (progName, startId, expr)
       Right (EForce expr')
 
     ELet ptrn expr1 expr2 -> do
-      expr1' <- applyModulesMap maps expr1
-      expr2' <- applyModulesMap maps expr2
+      expr1' <- applyModulesMap maps (progName, startId, expr1)
+      expr2' <- applyModulesMap maps (progName, startId, expr2)
       Right (ELet ptrn expr1' expr2')
 
     EAnno expr typ -> do
-      expr' <- applyModulesMap maps expr
+      expr' <- applyModulesMap maps (progName, startId, expr)
       Right (EAnno expr' typ)
 
     EIAbs ivar expr -> do
-      expr' <- applyModulesMap maps expr
+      expr' <- applyModulesMap maps (progName, startId, expr)
       Right (EIAbs ivar expr')
 
     EIApp expr i -> do
-      expr' <- applyModulesMap maps expr
+      expr' <- applyModulesMap maps (progName, startId, expr)
       Right (EIApp expr' i)
 
     EConst c -> Right (EConst c)
 
     EAssume expr typ -> do
-      expr' <- applyModulesMap maps expr
+      expr' <- applyModulesMap maps (progName, startId, expr)
       Right (EAssume expr' typ)
 
 -- Raises a run time error in case a definition appears in more than one module
 -- and we are unsure about which one to use.
 -- For now, I am not checking if the id is in the form module.name because
 -- I dont even know if a function can be called like that in .pq
-searchDefinition :: ModulesMap -> VariableId -> Either RuntimeError (Maybe TopLevelDefinition)
-searchDefinition maps varId =
-  -- a list of (mod_name, Expr) represen ting when defs are found
-  let foundDefinitions = [(moduleName, tldef) | (moduleName, defsMap) <- M.toList maps, Just tldef <- [M.lookup varId defsMap]]
+searchDefinition :: ModulesMap -> String -> VariableId -> VariableId -> Either RuntimeError (Maybe (String, TopLevelDefinition))
+-- modName is used to default to the user defined definition
+-- defName is the name of the definition in which we are trying to substitute in
+-- x is the name of the definition that we are looking for
+searchDefinition maps userModName defName x =
+  -- trace ("\nSearching a definition for EVar "++x++", found inside the definition '"++defName++"' in the module '"++userModName++"'.")$
+  -- a list of (mod_name, Expr) representing the found definitions
+  let foundDefinitions = [(moduleName, tldef) | (moduleName, defsMap) <- M.toList maps, Just tldef <- [M.lookup x defsMap]]
   in case foundDefinitions of
-    [] -> Right Nothing
-    [(_, def)] -> Right (Just def)
-    defs -> Left $ RuntimeError err
-      where
-        err = "Multiple definitions found for " ++ varId 
-            ++ ".\nIt is defined in the following modules:\n"
-            ++ (intercalate ",\n" (map fst defs))
+    [] -> 
+      -- trace (
+      --   "> No definition found for "++x
+      -- )$
+      Right Nothing
+    [(moduleName, tldef)] -> 
+      -- trace (
+      --   "> FOUND: Using "++x++" from "++moduleName
+      -- )$
+      Right (Just (moduleName, tldef))
+    defs -> -- if more than one definition is found and the name of the variable to look for
+            -- is the same as the name if the definition itself, than we have to look for the 
+            -- 'true' definition on another module, otherwise we substitute infinitely.
+       
+      -- remove found definitions in which defName == x
+      case filter (\(moduleName, _) -> moduleName /= userModName) defs of
+          [] -> Left $ RuntimeError "A definition is defined using itself recursively."
+          -- if we only have one definition left use it,
+          [(moduleName, tldef)] -> 
+            -- trace (
+            --   "> FOUND: Using "++x++" from "++moduleName
+            -- )$
+            Right (Just (moduleName, tldef))
+          -- otherwise try to default to the user-defined one
+          filteredDefs -> case lookup userModName filteredDefs of
+            Just tldef -> 
+              -- trace (
+              --   "> FOUND: Using "++x++" from "++userModName
+              -- )$
+              Right (Just (userModName, tldef))
+            -- more than one definition in the libraries. Unsure on which one to use
+            Nothing -> Left $ RuntimeError err
+              where
+                err = "Multiple definitions found for " ++ x 
+                    ++ ".\nIt is defined in the following modules:\n"
+                    ++ (intercalate ",\n" (map fst defs))
 
 wrapExpr :: Expr -> [Pattern] -> Maybe Type -> Expr
 -- I dont know if args can be empty and the type not be Nothing, and viceversa
-wrapExpr e [] _= e
+wrapExpr e [] _ = e
 wrapExpr e _ Nothing = e
 -- Analyze the type along with the patterns -> How do I extract the types corresponding to the patterns?
-wrapExpr e (p:ps) _ = EAbs undefined TUnit (wrapExpr e ps undefined) -- TODO
+wrapExpr expr ptrn (Just typ) = case (ptrn, typ) of
+  _ -> undefined -- TODO
