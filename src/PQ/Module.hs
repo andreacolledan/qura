4981ab56d9811a7a7ff73fe5ea0280a893f6a7c3
Andrea Colledan <and.colledan@gmail.com>
2025-09-01 22:34:55 +0200

    make analyzer step return type-annotated module

--- src/PQ/Module.hs
+++ src/PQ/Module.hs
@@ -28,3 +28,9 @@ data Module = Module {
 
 instance Pretty Module where
   pretty (Module name exports imports tldefs) = unwords $ map prettyTopLevelDefinition tldefs
+
+toTypeBindings :: Module -> [(VariableId, Type)]
+toTypeBindings (Module _ _ _ tldefs) = foldr collect [] tldefs
+  where
+    collect (TopLevelDefinition id _ (Just typ) _) acc = (id, typ) : acc
+    collect (TopLevelDefinition _ _ Nothing _) acc = acc
